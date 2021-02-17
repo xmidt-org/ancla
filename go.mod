bb64688adfde9f7393eccf46e733fb837179ce7c
Joel Unzain <joel_unzain@comcast.com>
2021-02-17 15:42:50 -0800

    Add existing code

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/xmidt-org/ancla
+
+go 1.15
+
+require (
+	github.com/go-kit/kit v0.10.0
+	github.com/stretchr/testify v1.7.0
+	github.com/xmidt-org/argus v0.3.10-0.20201217204602-66f69b12c498
+	github.com/xmidt-org/bascule v0.9.0
+	github.com/xmidt-org/webpa-common v1.11.5-0.20210120003553-3d03d7329aee
+)
---
+++ metrics.go
@@ -0,0 +1,42 @@
+package xwebhook
+
+import (
+	"github.com/go-kit/kit/metrics"
+	"github.com/xmidt-org/argus/chrysom"
+	"github.com/xmidt-org/webpa-common/xmetrics"
+)
+
+// Names
+const (
+	PollCounter          = "webhook_polls_total"
+	WebhookListSizeGauge = "webhook_list_size_value"
+)
+
+// Labels
+const (
+	OutcomeLabel = "outcome"
+)
+
+// Label Values
+const (
+	SuccessOutcome = "success"
+	FailureOutcome = "failure"
+)
+
+// Metrics returns the Metrics relevant to this package
+func Metrics() []xmetrics.Metric {
+	metrics := []xmetrics.Metric{
+		{
+			Name: WebhookListSizeGauge,
+			Type: xmetrics.GaugeType,
+			Help: "Size of the current list of webhooks.",
+		},
+	}
+	metrics = append(metrics, chrysom.Metrics()...)
+	return metrics
+}
+
+type measures struct {
+	pollCount       metrics.Counter
+	webhookListSize metrics.Gauge
+}
