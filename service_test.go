668664d1116af87662e3ef4e9aa7f416d0bbae1d
Joel Unzain <joel_unzain@comcast.com>
2021-02-25 09:45:01 -0800

    add test for allWebhooks

--- service_test.go
+++ service_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"io/ioutil"
 	"testing"
+	"time"
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/metrics/provider"
@@ -11,6 +12,38 @@ import (
 	"github.com/stretchr/testify/mock"
 	"github.com/xmidt-org/argus/chrysom"
 	"github.com/xmidt-org/argus/store"
+)
+
+var (
+	refTime              = getRefTime()
+	migrationWebhookZero = Webhook{
+		Address: "http://webhook-requester-to-sns.example",
+		Config: WebhookConfig{
+			URL: "http://events-webhook0-here.example",
+		},
+		Until: refTime,
+	}
+	migrationWebhookTwo = Webhook{
+		Address: "http://webhook-requester-to-sns.example",
+		Config: WebhookConfig{
+			URL: "http://events-webhook2-here.example",
+		},
+		Until: refTime.Add(8 * time.Second),
+	}
+	webhookZero = Webhook{
+		Address: "http://webhook-requester-to-argus.example",
+		Config: WebhookConfig{
+			URL: "http://events-webhook0-here.example",
+		},
+		Until: refTime.Add(5 * time.Second),
+	}
+	webhookOne = Webhook{
+		Address: "http://webhook-requester-to-argus.example",
+		Config: WebhookConfig{
+			URL: "http://events-webhook1-here.example",
+		},
+		Until: refTime.Add(10 * time.Second),
+	}
 )
 
 type validateTestconfig struct {
@@ -117,9 +150,132 @@ func TestAdd(t *testing.T) {
 	}
 }
 
-// Test AllWebhooks
 func TestAllWebhooks(t *testing.T) {
-	//TODO:
+	type getItemsMockResp struct {
+		Items chrysom.Items
+		Err   error
+	}
+
+	type testCase struct {
+		Description          string
+		Owner                string
+		CaptureMigratedItems bool
+		CaptureItems         bool
+		MigrationItemsResp   getItemsMockResp
+		ItemsResp            getItemsMockResp
+		ExpectedWebhooks     []Webhook
+		ExpectedErr          error
+	}
+
+	migrationItemsResp := getItemsMockResp{
+		Items: getTestMigrationItems(),
+	}
+
+	itemsResp := getItemsMockResp{
+		Items: getTestItems(),
+	}
+
+	tcs := []testCase{
+		{
+			Description:          "Fetching migrated webhooks fails",
+			Owner:                "Owner",
+			CaptureMigratedItems: true,
+			CaptureItems:         false,
+			MigrationItemsResp: getItemsMockResp{
+				Err: errors.New("db failed"),
+			},
+			ItemsResp:   itemsResp,
+			ExpectedErr: errFailedMigratedWebhooksFetch,
+		},
+
+		{
+			Description:          "Fetching argus webhooks fails",
+			Owner:                "Owner",
+			CaptureMigratedItems: true,
+			CaptureItems:         true,
+			MigrationItemsResp:   migrationItemsResp,
+			ItemsResp: getItemsMockResp{
+				Err: errors.New("db failed"),
+			},
+			ExpectedErr: errFailedWebhooksFetch,
+		},
+		{
+			Description:          "Migration capture disabled. Webhooks fetch success",
+			Owner:                "Owner",
+			CaptureMigratedItems: false,
+			CaptureItems:         true,
+			MigrationItemsResp:   migrationItemsResp,
+			ItemsResp:            itemsResp,
+			ExpectedWebhooks:     getWebhooksFromArgusOnly(),
+		},
+		{
+			Description:          "Only Migrated webhooks",
+			Owner:                "Owner",
+			CaptureMigratedItems: true,
+			CaptureItems:         true,
+			MigrationItemsResp:   migrationItemsResp,
+			ItemsResp: getItemsMockResp{
+				Items: chrysom.Items{},
+			},
+			ExpectedWebhooks: getWebhooksFromMigrationOnly(),
+		},
+		{
+			Description:          "Success fetch from both sources",
+			Owner:                "Owner",
+			CaptureMigratedItems: true,
+			CaptureItems:         true,
+			MigrationItemsResp:   migrationItemsResp,
+			ItemsResp:            itemsResp,
+			ExpectedWebhooks:     getWebhooksFromMixedSources(),
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.Description, func(t *testing.T) {
+			assert := assert.New(t)
+			m := new(mockPushReader)
+			migrationCfg := &MigrationConfig{
+				Bucket: "webhooks",
+				Owner:  "migration-owner",
+			}
+			cfg := Config{}
+			if tc.CaptureMigratedItems {
+				cfg.Migration = migrationCfg
+			}
+
+			svc := service{
+				argus:  m,
+				logger: log.NewNopLogger(),
+				config: cfg,
+			}
+			if tc.CaptureMigratedItems {
+				m.On("GetItems", migrationCfg.Bucket, migrationCfg.Owner).
+					Return(tc.MigrationItemsResp.Items, tc.MigrationItemsResp.Err)
+			}
+
+			if tc.CaptureItems {
+				m.On("GetItems", svc.config.Bucket, tc.Owner).Return(tc.ItemsResp.Items, tc.ItemsResp.Err)
+			}
+
+			webhooks, err := svc.AllWebhooks(tc.Owner)
+
+			if tc.ExpectedErr != nil {
+				assert.True(errors.Is(err, tc.ExpectedErr))
+				assert.Empty(webhooks)
+			} else {
+				assert.EqualValues(tc.ExpectedWebhooks, webhooks)
+			}
+
+			if !tc.CaptureMigratedItems {
+				m.AssertNotCalled(t, "GetItems", migrationCfg.Bucket, migrationCfg.Owner)
+			}
+
+			if !tc.CaptureItems {
+				m.AssertNotCalled(t, "GetItems", svc.config.Bucket, tc.Owner)
+			}
+
+			m.AssertExpectations(t)
+		})
+	}
 }
 
 func getIncompleteButValidConfig() validateTestconfig {
@@ -167,4 +323,74 @@ func getInvalidConfig() validateTestconfig {
 			},
 		},
 	}
+}
+
+func getTestItems() chrysom.Items {
+	return chrysom.Items{
+		{
+			ID: "d73ec0f8e6137f50284453bf1da67f94659fae70cefef8745a94a638bab41b90",
+			Data: map[string]interface{}{
+				"registered_from_address": "http://webhook-requester-to-argus.example",
+				"config": map[string]interface{}{
+					"url": "http://events-webhook0-here.example",
+				},
+				"until": "2021-01-02T15:04:05Z",
+			},
+		},
+		{
+			ID: "900018a2bd769407338e49693d5e8dc91301a10620b79b3e5bffdc8791e43bc6",
+			Data: map[string]interface{}{
+				"registered_from_address": "http://webhook-requester-to-argus.example",
+				"config": map[string]interface{}{
+					"url": "http://events-webhook1-here.example",
+				},
+				"until": "2021-01-02T15:04:10Z",
+			},
+		},
+	}
+}
+
+func getTestMigrationItems() chrysom.Items {
+	return chrysom.Items{
+		{
+			ID: "d73ec0f8e6137f50284453bf1da67f94659fae70cefef8745a94a638bab41b90",
+			Data: map[string]interface{}{
+				"registered_from_address": "http://webhook-requester-to-sns.example",
+				"config": map[string]interface{}{
+					"url": "http://events-webhook0-here.example",
+				},
+				"until": "2021-01-02T15:04:00Z",
+			},
+		},
+		{
+			ID: "4f4908c2d545216b8996a701866ce1c0312bec2bf316e4cfecf2465634bf8398",
+			Data: map[string]interface{}{
+				"registered_from_address": "http://webhook-requester-to-sns.example",
+				"config": map[string]interface{}{
+					"url": "http://events-webhook2-here.example",
+				},
+				"until": "2021-01-02T15:04:08Z",
+			},
+		},
+	}
+}
+
+func getWebhooksFromArgusOnly() []Webhook {
+	return []Webhook{webhookZero, webhookOne}
+}
+
+func getWebhooksFromMigrationOnly() []Webhook {
+	return []Webhook{migrationWebhookZero, migrationWebhookTwo}
+}
+
+func getWebhooksFromMixedSources() []Webhook {
+	return []Webhook{webhookZero, webhookOne, migrationWebhookTwo}
+}
+
+func getRefTime() time.Time {
+	refTime, err := time.Parse(time.RFC3339, "2021-01-02T15:04:00Z")
+	if err != nil {
+		panic(err)
+	}
+	return refTime
 }
