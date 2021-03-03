0dc4a8218ddcb8f0ff6b363c6270a479e56881f6
Joel Unzain <joel_unzain@comcast.com>
2021-03-03 14:26:10 -0800

    add doc file

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Package ancla contains the XMiDT event webhook registry component. In addition
+to the main service, ancla provides off the shelf HTTP handlers for both
+adding and retrieving webhooks.
+*/
+package ancla
