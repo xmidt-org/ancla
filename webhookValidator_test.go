9abf345fb37a291fd7f5b90f292f73099e57ed00
mtrinh11 <mtrinh11@gmail.com>
2021-08-25 15:14:55 -0700

    Update webhookValidator_test.go
    Co-authored-by: Kristina Pathak <kmspring57@gmail.com>

--- webhookValidator_test.go
+++ webhookValidator_test.go
@@ -274,7 +274,7 @@ func TestRejectLoopback(t *testing.T) {
 		expectedErr error
 	}{
 		{
-			desc: "Non loopback URL Sucess",
+			desc: "Non loopback URL Success",
 			url:  "http://www.example.com:1030/software/index.html",
 		},
 		{
