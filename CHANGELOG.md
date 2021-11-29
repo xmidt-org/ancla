0d1c238463377c914d1ab00ef2fbfaa65bb25762
mtrinh11 <mtrinh11@gmail.com>
2021-11-29 13:03:53 -0800

    changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+- Changed errorEncoder to log errors. [#90](https://github.com/xmidt-org/ancla/pull/90)
 
 ## [v0.3.3]
 - Removed legacy logic condition for inserting a webhook. [#85](https://github.com/xmidt-org/ancla/pull/85)
--- transport.go
+++ transport.go
@@ -28,6 +28,7 @@ import (
 	"time"
 
 	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 	kithttp "github.com/go-kit/kit/transport/http"
 	"github.com/spf13/cast"
 	"github.com/xmidt-org/httpaux/erraux"
@@ -231,7 +232,7 @@ func errorEncoder(getLogger GetLoggerFunc) kithttp.ErrorEncoder {
 
 		logger := getLogger(ctx)
 		if logger != nil && code != http.StatusNotFound {
-			logger.Log("sending non-200, non-404 response", err, code)
+			logger.Log("sending non-200, non-404 response", level.Key(), level.ErrorValue(), err, code)
 		}
 
 		w.WriteHeader(code)
