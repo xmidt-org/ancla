4403ded632666310b2626f7ed95fdba9a0249003
maura fortino <maurafortino@gmail.com>
2024-11-07 16:21:54 -0500

    updated MeasuresIn struct

--- chrysom/metrics.go
+++ chrysom/metrics.go
@@ -37,7 +37,8 @@ type Measures struct {
 // MeasuresIn is an uber/fx parameter with the webhook registration counter.
 type MeasuresIn struct {
 	fx.In
-	Measures
+	WebhookListSizeGauge prometheus.Gauge       `name:"webhook_list_size"`
+	PollsTotalCounter    *prometheus.CounterVec `name:"chrysom_polls_total"`
 }
 
 // NewMeasures realizes desired metrics.
