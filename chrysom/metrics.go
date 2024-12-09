bcd326dfa238bdd469d365c6df7d1295ed0d0bcf
Owen Cabalceta <Owen_Cabalceta@comcast.com>
2024-12-09 15:50:27 -0500

    refactor: simplify inner dependences and structure for easier setup with and without fx
    - add ability for clients to setup an ancla client with only `anclafx.Provide()`
    - simplify calls to setup ancla without fx
    - simplify inner dependences and structure

--- chrysom/metrics.go
+++ chrysom/metrics.go
@@ -3,12 +3,6 @@
 
 package chrysom
 
-import (
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/xmidt-org/touchstone"
-	"go.uber.org/fx"
-)
-
 // Names
 const (
 	WebhookListSizeGaugeName = "webhook_list_size"
@@ -27,44 +21,3 @@ const (
 	SuccessOutcome = "success"
 	FailureOutcome = "failure"
 )
-
-// Measures describes the defined metrics that will be used by clients.
-type Measures struct {
-	WebhookListSizeGauge prometheus.Gauge       `name:"webhook_list_size"`
-	PollsTotalCounter    *prometheus.CounterVec `name:"chrysom_polls_total"`
-}
-
-// MeasuresIn is an uber/fx parameter with the webhook registration counter.
-type MeasuresIn struct {
-	fx.In
-
-	WebhookListSizeGauge prometheus.Gauge       `name:"webhook_list_size"`
-	PollsTotalCounter    *prometheus.CounterVec `name:"chrysom_polls_total"`
-}
-
-// NewMeasures realizes desired metrics.
-func NewMeasures(in MeasuresIn) Measures {
-	return Measures{
-		WebhookListSizeGauge: in.WebhookListSizeGauge,
-		PollsTotalCounter:    in.PollsTotalCounter,
-	}
-}
-
-// Metrics returns the Metrics relevant to this package
-func ProvideMetrics() fx.Option {
-	return fx.Options(
-		fx.Provide(NewMeasures),
-		touchstone.Gauge(
-			prometheus.GaugeOpts{
-				Name: WebhookListSizeGaugeName,
-				Help: WebhookListSizeGaugeHelp,
-			}),
-		touchstone.CounterVec(
-			prometheus.CounterOpts{
-				Name: PollsTotalCounterName,
-				Help: PollsTotalCounterHelp,
-			},
-			OutcomeLabel,
-		),
-	)
-}
